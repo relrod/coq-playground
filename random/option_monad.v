701cae6cc0edda7c1c0c743019f94b7dc403c73f
Ricky Elrod <ricky@elrod.me>
2015-01-06 18:37:33 -0500

    Probably a more accurate encoding. Type checks, so it has to be right? :P
    Signed-off-by: Ricky Elrod <ricky@elrod.me>

--- random/option_monad.v
+++ random/option_monad.v
@@ -1,3 +1,13 @@
+Class monad (m : Type -> Type) :=
+  { bind : forall {a b}, m a -> (a -> m b) -> m b
+  ; ret  : forall {a}, a -> m a
+
+  ; law_1 : forall a b (f : a -> m b) (x : a), bind (ret x) f = f x
+  ; law_2 : forall a (x : m a), bind x ret = x
+  ; law_3 : forall a b c (x : m a) (f : a -> m b) (g : b -> m c),
+              bind (bind x f) g = bind x (fun x => bind (f x) g)
+  }.
+
 Fixpoint option_map {A B : Type} (f : A -> B) (o : option A): option B :=
   match o with
     | None => None
@@ -26,27 +36,18 @@ Notation "f >>= x" := (option_flatmap f x) (at level 50).
 
 Definition option_return {A : Type} (o : A) := Some o.
 
-Theorem monad_left_id : forall {A B : Type} (x : A) (f : A -> option B),
-                          option_flatmap (option_return x) f = f x.
-Proof.
-  intros.
-  reflexivity.
-Qed.
-
-Theorem monad_right_id : forall {A: Type} (x : option A),
-                           x >>= option_return = x.
-Proof.
-  intros.
-  destruct x.
-  reflexivity.
-  reflexivity.
-Qed.
-
-Theorem monad_assoc : forall {A B C : Type} (x : option A) (f : A -> option B) (g : B -> option C),
-                        (x >>= f) >>= g = x >>= fun x => f x >>= g.
-Proof.
-  intros.
-  destruct x.
-  reflexivity.
-  reflexivity.
+Instance option_monad : monad option :=
+  {
+      bind := @option_flatmap
+    ; ret := @Some
+  }.
+constructor.
+intros.
+destruct x.
+reflexivity.
+reflexivity.
+intros.
+destruct x.
+reflexivity.
+reflexivity.
 Qed.
